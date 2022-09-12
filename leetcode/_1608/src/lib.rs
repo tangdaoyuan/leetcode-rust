23bc5a57496d7d0bff02aecb21751c51efac91ec
tangdaoyuan <1197633750@qq.com>
2022-09-12 21:23:11 +0800

    feat: 1608

---
+++ leetcode/_1608/src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+
+// https://leetcode.cn/problems/special-array-with-x-elements-greater-than-or-equal-x/
+struct Solution;
+
+impl Solution {
+    pub fn special_array(_nums: Vec<i32>) -> () {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+}
