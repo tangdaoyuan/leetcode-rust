976a25643964c7fc6aff51b19d52ab12b136dec3
tangdaoyuan <1197633750@qq.com>
2022-09-13 23:00:34 +0800

    feat: 1608 special array

--- leetcode/_1608/src/lib.rs
+++ leetcode/_1608/src/lib.rs
@@ -1,15 +1,43 @@
 #![allow(dead_code)]
 
-// https://leetcode.cn/problems/special-array-with-x-elements-greater-than-or-equal-x/
+/// Copy from: https://leetcode.cn/problems/special-array-with-x-elements-greater-than-or-equal-x/
+
 struct Solution;
 
 impl Solution {
-    pub fn special_array(_nums: Vec<i32>) -> () {
+    pub fn special_array(nums: Vec<i32>) -> i32 {
+        let mut _nums: Vec<i32> = nums[0..].to_vec();
+        _nums.sort();
+        let len = _nums.len();
+        if len == 0 {
+            return 0;
+        }
 
+        let mut ret: i32 = -1;
+        for i in (0..len).rev() {
+            let val = i as i32;
+            if _nums[len - 1 - i] >= val + 1 {
+                if len - 1 - i == 0 {
+                    ret = val + 1;
+                    break;
+                } else if _nums[len - i - 2] < val + 1 {
+                    ret = val + 1;
+                    break;
+                }
+            }
+        }
+        ret
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::Solution;
+
+    #[test]
+    fn special_array_works() {
+        let vec = vec![3, 5];
+        let result = Solution::special_array(vec);
+        assert_eq!(result, 2)
+    }
 }
