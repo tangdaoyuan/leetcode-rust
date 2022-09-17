76ccfc70041a8b156559043f96699e4f0b6447df
tangdaoyuan <1197633750@qq.com>
2022-09-17 22:32:38 +0800

    chore: format

--- leetcode/_599/src/lib.rs
+++ leetcode/_599/src/lib.rs
@@ -11,12 +11,12 @@ impl Solution {
         let mut ind = 0;
         let mut ret = vec![];
         let mut min = i64::MAX;
-        
+
         for item in &list1 {
             if !map.contains_key(item) {
                 map.insert(item, ind);
             }
-            ind+=1;
+            ind += 1;
         }
 
         ind = 0;
@@ -24,7 +24,7 @@ impl Solution {
             if map.contains_key(item) {
                 let val = match map.get(item) {
                     Some(&v) => v,
-                    None => -1
+                    None => -1,
                 };
                 if val + ind < min {
                     ret.clear();
@@ -33,9 +33,8 @@ impl Solution {
                 } else if val + ind == min {
                     ret.push(String::from(item));
                 }
-
             }
-            ind+=1;
+            ind += 1;
         }
         return ret;
     }
@@ -47,14 +46,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let list1 = vec![String::from("Shogun"),String::from("Tapioca Express"), String::from("Burger King"),String::from("KFC")];
+        let list1 = vec![
+            String::from("Shogun"),
+            String::from("Tapioca Express"),
+            String::from("Burger King"),
+            String::from("KFC"),
+        ];
         let list2 = vec![
             String::from("Piatti"),
             String::from("The Grill at Torrey Pines"),
             String::from("Hungry Hunter Steakhouse"),
-            String::from("Shogun")
+            String::from("Shogun"),
         ];
-        
+
         assert_eq!(Solution::find_restaurant(list1, list2), ["Shogun"]);
     }
 }
